3ac60491ed082782c60a593b614af209e603025f
Haidar_Lenovo <haidar.y.alsalama@gmail.com>
2023-03-13 10:34:17 +0300

    lenovo

--- js/main.js
+++ js/main.js
@@ -2,6 +2,14 @@
 
 let menuButton = document.querySelector('.menuButton');
 let menu = document.querySelector('.menu');
-menuButton.addEventListener('click',function () {
-    menu.classList.toggle('showMenu');
-});
+
+if(menuButton != null) {
+    menuButton.addEventListener('click',function () {
+        menu.classList.toggle('showMenu');
+    });
+}
+
+window.onresize = () => {
+    if(menu.classList.contains('showMenu') && window.innerWidth > 1056)
+    menu.classList.remove('showMenu');
+}
